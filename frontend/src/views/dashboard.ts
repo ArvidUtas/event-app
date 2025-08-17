74d57c5b395586afa3378275854872050a5da705
ArvidUtas <arvidu@hotmail.com>
2025-08-17 20:17:11 +0200

    FE: searchEvent connected with BE for keyword, organisedBy, venue. So far only console.log(). Start and end times not working on FE.

--- frontend/src/views/dashboard.ts
+++ frontend/src/views/dashboard.ts
@@ -3,6 +3,19 @@ export function showDashboard(): string {
     <h1>Welcome!</h1>
     <button class="btn btn-success" id="create-event">Create New Event</button>
     <button class="btn btn-danger" id="log-out">Log Out</button>
+    <h2 class="mt-5">Find Event</h2>
+    <form class="form-control border-0" id="search-event-form">
+        <input type="text" class="form-control w-25 mt-2" name="keyword" placeholder="Keyword:">
+        <input type="text" class="form-control w-25 mt-2" name="organisedBy" placeholder="Organised by:">
+        <input type="text" class="form-control w-25 mt-2" name="venue" placeholder="Venue:">
+        <input type="text" class="form-control w-25 mt-2" name="city" placeholder="City:">
+        <label for="start-time" class="form-label mt-2">Start time:</label>
+        <input type="datetime-local" class="form-control w-25" name="startTime"/>
+        <label for="end-time" class="form-label mt-2">End time:</label>
+        <input type="datetime-local" class="form-control w-25" name="endTime"/>
+        <br>
+        <button type="submit" class="btn btn-success border">Search</button>
+    </form>
   `;
 }
 
@@ -11,9 +24,49 @@ export function setupDashboardListeners(){
   logOutBtn?.addEventListener("click", logOut);
   const createEventBtn = document.getElementById("create-event") as HTMLButtonElement;
   createEventBtn?.addEventListener("click", () => {window.location.href = "/create-event"});
+  const form = document.getElementById("search-event-form") as HTMLFormElement;
+  if (!form) {
+        console.error("Form not found");
+        return;        
+    }
+  form.addEventListener("submit", postSearch);
 }
 
 function logOut(){
   sessionStorage.removeItem("token");
   window.location.href="/login";
+}
+
+function postSearch(e:Event){
+  e.preventDefault();
+  const form = e.target as HTMLFormElement;
+  if (!form){
+      console.error("Form not found");
+      return;
+  }
+  const formData = new FormData(form);
+  const params: Record<string, string> = {};
+  
+  formData.forEach((value, key) => {
+    if (value){
+      params[key] = String(value);
+    }
+  });
+  console.log(params);
+  
+  const queryString = new URLSearchParams(params).toString();
+  
+  fetch(`http://localhost:8080/events/search?${queryString}`)
+    .then((res)=>res.json())
+    .then((data) => console.log(data));  
+}
+
+//possibly not needed:
+interface SearchFormData {
+  keyword?: string;
+  organisedBy?: string;
+  venue?: string;
+  city?: string;
+  startTime?: string;
+  endTime?: string;
 }
